7909ed5f490ac528b611d5dac5bdcdb7f6af2cbc
Brian Baggs <brianbaggs@hotmail.com>
2026-08-15 12:56:40 -0300

    Update eslint to 10.8.1 and update eslint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,62 +1,146 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require('eslint/config')
+const globals = require('globals')
+const js = require('@eslint/js')
+const tsParser = require('@typescript-eslint/parser')
+const tsPlugin = require('@typescript-eslint/eslint-plugin')
+const reactRefreshPackage = require('eslint-plugin-react-refresh')
+const reactHooks = require('eslint-plugin-react-hooks')
 
-const globals = require("globals");
+const reactRefresh = reactRefreshPackage.reactRefresh
 
-const {
-    fixupConfigRules,
-} = require("@eslint/compat");
+module.exports = defineConfig([
+  {
+    files: ['src/**/*.{js,jsx,ts,tsx}'],
 
-const tsParser = require("@typescript-eslint/parser");
-const reactRefresh = require("eslint-plugin-react-refresh");
-const reactHooks = require("eslint-plugin-react-hooks");
-const js = require("@eslint/js");
-
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
+      globals: {
+        ...globals.browser,
+        React: 'readonly',
+      },
 
-        parser: tsParser,
+      parser: tsParser,
+
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
 
-    extends: fixupConfigRules(compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-    )),
-
     plugins: {
-        "react-refresh": reactRefresh,
-        "react-hooks": reactHooks,
+      '@typescript-eslint': tsPlugin,
+      'react-refresh': reactRefresh.plugin,
+      'react-hooks': reactHooks,
     },
 
     rules: {
-        "react-refresh/only-export-components": ["warn", {
-            allowConstantExport: true,
-        }],
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+
+      'no-undef': 'off',
+
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
-}, globalIgnores([
-    "**/dist",
-    "**/.eslintrc.cjs",
-    "**/eslint.config.cjs",
-    "**/coverage",
-    "**/.history",
-    "**/node_modules",
-    "**/playwright-report",
-    "**/test-results",
-])]);
+  },
+
+  {
+    files: [
+      'playwright.config.{js,ts}',
+      'server/**/*.{js,ts}',
+      'spec/**/*.{js,jsx,ts,tsx}',
+    ],
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+
+      'no-undef': 'off',
+    },
+  },
+
+  {
+    files: ['src/test/**/*.{js,jsx,ts,tsx}'],
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        React: 'readonly',
+      },
+
+      parser: tsParser,
+
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-refresh': reactRefresh.plugin,
+      'react-hooks': reactHooks,
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+
+      'no-undef': 'off',
+
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+    },
+  },
+
+  globalIgnores([
+    '**/dist',
+    '**/.eslintrc.cjs',
+    '**/eslint.config.cjs',
+    '**/coverage',
+    '**/.history',
+    '**/node_modules',
+    '**/playwright-report',
+    '**/test-results',
+  ]),
+])
